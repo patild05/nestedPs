4c6c91cdd6b174ab0413f243e36966f135845dea
patild05 <dipak.patil.it@gmail.com>
2018-02-21 12:14:42 +0000

    Update Trigger.ps1

--- Trigger.ps1
+++ Trigger.ps1
@@ -1,6 +1,5 @@
 Write-Output "This is trigger"
 
 $path = Split-Path $MyInvocation.InvocationName 
-& "$path\level1.ps1"
 Write-Output $path
-invoke-expression -Command $path
+invoke-expression -Command $path\level1.ps1
